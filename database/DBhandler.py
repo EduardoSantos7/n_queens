6dc236bb4c850ac5bb21590ba918d411e9c8a33d
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-10-16 14:19:45 -0500

    Env variables

--- database/DBhandler.py
+++ database/DBhandler.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Integer
@@ -6,6 +7,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects import postgresql
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 base = declarative_base()
@@ -25,7 +30,12 @@ class Solution(base):
 
 
 class DBhandler:
-    db_string = "postgresql://reysantos7:05120714@localhost:5437/test"
+    db = os.getenv("POSTGRES_DB")
+    port = os.getenv("POSTGRES_PORT")
+    host = os.getenv("POSTGRES_HOST")
+    username = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    db_string = f"postgresql://{username}:{password}@{host}:{port}/{db}"
 
     def __init__(self):
         self.db = create_engine(self.db_string)
