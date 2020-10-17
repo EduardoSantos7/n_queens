0ad045d223687e17ce5ebe83d68a0335c6426356
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-10-17 15:58:54 -0500

    continuos main

--- main.py
+++ main.py
@@ -37,5 +37,11 @@ def solve_n_queens(queens, use_db=False):
 
 
 if __name__ == "__main__":
-    n = int(input("Type a number N (board of NxN and N queens): "))
-    solve_n_queens(n)
+    user_input = input("Type a number N (board of NxN and N queens) or q to finish: ")
+    while user_input != 'q':
+        if user_input.isdecimal():
+            n = int(user_input)
+            solve_n_queens(n)
+        else:
+            print("Please type only numbers")
+        user_input = input("Type a number N (board of NxN and N queens) or q to finish: ")
